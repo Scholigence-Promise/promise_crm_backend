035da529bfda9c2125244b1fb1e347b91f1bd737
Mezba Sageer <mezbasageer19@gmail.com>
2026-03-30 11:58:05 +0530

    role_model_updated

--- api/models.py
+++ api/models.py
@@ -10,17 +10,36 @@ class CustomUser(models.Model):
     def __str__(self):
         return self.user.email
     
+     
+#Role Model 
+class Role(models.Model):
+    ROLE_CHOICES = [
+        ('superadmin', 'Superadmin'),
+        ('general manager', 'General manager'),
+        ('operations manager', 'Operations manager'),
+        ('department head','Department head'),
+        ('underwriter','Underwriter'),
+        ('sales manager','Sales manager'),
+        ('telecallers','Telecallers'),
+        ('customers','Customers')
+    ]
 
-#  Profile model
+    name = models.CharField(max_length=20, choices=ROLE_CHOICES, unique=True)
+
+    def __str__(self):
+        return self.get_name_display()
+    
+#Profile model
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-
     name = models.CharField(max_length=100)
     bio = models.TextField(blank=True, null=True)
     profile_pic = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    role = models.ForeignKey(Role, on_delete=models.SET_NULL, null=True, blank=True)
+
 
     def __str__(self):
-        return self.user.username 
+        return self.user.username
     
 from django.db.models.signals import post_save
 from django.dispatch import receiver
